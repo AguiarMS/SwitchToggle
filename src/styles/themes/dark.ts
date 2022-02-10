ba78b1877b919c2df6b001d64115c984f4c92614
Vitor Aguiar <v.aguiar@hotmail.com>
2022-02-10 17:48:11 -0300

    comint

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#E55381',
+    secundary: '#EFA9AE',
+
+    background: '#222',
+    text: '#fff'
+  }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#E55381',
+    secundary: '#EFA9AE',
+   
+    background: '#f5f5f5',
+    text: '#333'
+  }
+}
